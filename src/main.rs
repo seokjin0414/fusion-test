59625833d2f18cd409fd0e2d81f7790d7c33e649
shin <sars21@hanmail.net>
2025-06-02 15:50:58 +0900

    test ing...

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,3 @@
-use std::collections::HashMap;
 use std::sync::Arc;
 use anyhow::{anyhow, Result};
 use aws_config::{
@@ -6,12 +5,91 @@ use aws_config::{
     BehaviorVersion, 
     SdkConfig
 };
+use chrono::{DateTime, Utc};
+use datafusion::arrow::array::{Float64Array, RecordBatch, StringArray, TimestampNanosecondArray};
+use datafusion::arrow::datatypes::{DataType, Field, Schema, TimeUnit};
+use datafusion::dataframe::{DataFrame, DataFrameWriteOptions};
+use datafusion::datasource::MemTable;
+use datafusion::prelude::SessionContext;
+use datafusion_iceberg::DataFusionTable;
+use dotenvy::var;
 // use aws_sdk_s3tables;
 use iceberg_rust::catalog::Catalog;
 use iceberg_rust::catalog::tabular::Tabular;
 use iceberg_rust::object_store::ObjectStoreBuilder;
 use iceberg_rust::spec::identifier::Identifier;
 use iceberg_s3tables_catalog::S3TablesCatalog;
+use serde_derive::Deserialize;
+use uuid::Uuid;
+
+#[derive(Deserialize, Debug, Clone)]
+struct DataSet {
+    pub measurement_point_id: Uuid,
+    pub value: Option<f64>,
+    pub recorded_at: DateTime<Utc>,
+}
+
+impl DataSet {
+    pub fn test_set() -> Result<Vec<DataSet>> {
+        let mut rdr = csv::Reader::from_path("src/test_set.csv")
+            .map_err(|e| anyhow!("fail to read test_set.csv: {:?}", e))?;
+
+        let mut vec: Vec<DataSet> = Vec::new();
+        for result in rdr.deserialize() {
+            let record: DataSet = result
+                .map_err(|e| anyhow!("fail to deserialize: {:?}", e))?;
+            vec.push(record);
+        }
+
+        Ok(vec)
+    }
+}
+
+fn data_set_to_record_batch(data: &[DataSet]) -> Result<RecordBatch> {
+    let schema = Arc::new(Schema::new(vec![
+        Field::new("measurement_point_id", DataType::Utf8, false),
+        Field::new("value", DataType::Float64, true),
+        Field::new("recorded_at", DataType::Timestamp(TimeUnit::Nanosecond, None), false),
+    ]));
+
+    let id_array = StringArray::from_iter(
+        data.iter().map(|d|  Some(d.measurement_point_id.to_string()))
+    );
+    let value_array = Float64Array::from_iter(
+        data.iter().map(|d| d.value)
+    );
+    let recorded_array = TimestampNanosecondArray::from_iter(
+        data.iter().map(|d| d.recorded_at.timestamp_nanos_opt())
+    );
+
+    let batch = RecordBatch::try_new(
+        schema,
+        vec![
+            Arc::new(id_array),
+            Arc::new(value_array),
+            Arc::new(recorded_array),
+        ],
+    ).map_err(|e| anyhow!("fail to create record batch: {:?}", e))?;
+    
+    Ok(batch)
+}
+
+async fn record_batch_to_dataframe(
+    batch: RecordBatch,
+    ctx: &SessionContext,
+) -> Result<DataFrame> {
+    let schema = batch.schema();
+    let table = MemTable::try_new(schema, vec![vec![batch]])?;
+    
+    ctx.register_table("tmp_mem_table", Arc::new(table))
+        .map_err(|e| anyhow!("fail to register table: {:?}", e))?;
+    
+    let df = ctx.table("tmp_mem_table")
+        .await
+        .map_err(|e| anyhow!("fail to get table: {:?}", e))?;
+ 
+    Ok(df)
+}
 
 async fn init_table() -> Result<Tabular> {
     // AWS config
@@ -25,13 +103,19 @@ async fn init_table() -> Result<Tabular> {
     
     // let client = aws_sdk_s3tables::Client::new(&sdk_config);
     
-    // Catalog 생성
-    let catalog_arn = "";
-    let catalog = S3TablesCatalog::new(&sdk_config, catalog_arn, ObjectStoreBuilder::s3())?;
+    // Catalog
+    let catalog_arn = var("S3_TABLES_CATALOG_ARN")
+        .map_err(|e| anyhow!("fail to get S3_TABLES_CATALOG_ARN: {:?}", e))?;
+    
+    let catalog = S3TablesCatalog::new(&sdk_config, &catalog_arn, ObjectStoreBuilder::s3())?;
     let arc_catalog: Arc<dyn Catalog> = Arc::new(catalog);
 
-    // 테이블 로딩
-    let table_identifier=  Identifier::new(&["v1".to_string()], "test");
+    // Table
+    let namespace = var("NAMESPACE")
+        .map_err(|e| anyhow!("fail to get NAMESPACE: {:?}", e))?;
+    let test_table = var("TEST_TABLE")
+        .map_err(|e| anyhow!("fail to get TEST_TABLE: {:?}", e))?;
+    let table_identifier=  Identifier::new(&[namespace], &test_table);
     let table = arc_catalog.load_tabular(&table_identifier).await?;
     
     Ok(table)
@@ -40,7 +124,18 @@ async fn init_table() -> Result<Tabular> {
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    println!("Hello, world!");
+    let table = init_table().await?;
+    let df_table = Arc::new(DataFusionTable::from(table));
+    
+    let test_data_set = DataSet::test_set()?;
+    
+    let batch = data_set_to_record_batch(&test_data_set)?;
 
+    let ctx = SessionContext::new();
+    let df = record_batch_to_dataframe(batch, &ctx).await?;
+
+    
+    // df.write_table("test", DataFrameWriteOptions::default()).await?;
+    
     Ok(())
 }
